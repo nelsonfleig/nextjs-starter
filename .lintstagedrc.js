2abce9a74a1e7f0d6db2e814a9047215c64ad44e
Nelson Fleig <nfleig@hotmail.com>
2022-01-23 18:24:14 +0100

    added prettify to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   // Run type-check on changes to TypeScript files
-  "**/*.ts?(x)": () => "yarn check-types",
+  '**/*.ts?(x)': () => 'yarn check-types',
   // Run ESLint on changes to JavaScript/TypeScript files
-  "**/*.(ts|js)?(x)": (filenames) => `yarn lint . ${filenames.join(" ")}`,
+  '**/*.(ts|js)?(x)': filenames => `yarn lint . ${filenames.join(' ')}`,
+  // Prettify on changes to JavaScript/TypeScript files
+  '**/*.(ts|js)?(x)': () => `yarn format`,
 };
