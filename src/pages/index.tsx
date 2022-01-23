5872960cf046e27d23417d92c66dfc253cac390e
Nelson Fleig <nfleig@hotmail.com>
2022-01-23 18:50:09 +0100

    testing lint in ci

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,6 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
-const test = 'hi';
-console.log('forbidden console log');
-
 const Home: NextPage = () => (
   <div className={styles.container}>
     <Head>
