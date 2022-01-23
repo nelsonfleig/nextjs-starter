a3c65cda7d92277db1f569524278dbe54d74c423
Nelson Fleig <nfleig@hotmail.com>
2022-01-23 18:08:48 +0100

    added lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Run type-check on changes to TypeScript files
+  "**/*.ts?(x)": () => "yarn check-types",
+  // Run ESLint on changes to JavaScript/TypeScript files
+  "**/*.(ts|js)?(x)": (filenames) => `yarn lint . ${filenames.join(" ")}`,
+};
